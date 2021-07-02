3740afbe67b703da0c767ca771b5b9de14b6db8a
McKayla Lankau <lankaukk@yahoo.com>
2021-07-02 01:10:55 -0400

    can create user

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        @user = User.find_by(email: params[:user][:email])
+        return redirect_to '/login' unless @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+        redirect_to '/'
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to '/'
+    end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,26 @@
+  
 class UsersController < ApplicationController
-
-    def  show
-
-    end
-
-    def new 
+    def new
 
     end
 
-    def create 
-
+    def create
+        user = User.new(user_params)
+        if user.save
+            session[:user_id] = user.id
+            redirect_to '/stories'
+        else
+            redirect_to '/users/new'
+        end
     end
 
+    def show
+
+    end
+     
     private
-
+     
     def user_params
-
+        params.require(:user).permit(:first_name, :last_name, :email, :bio, :password, :password_confirmation)
     end
 end
